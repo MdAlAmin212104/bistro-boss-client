6728c575cbb4e2d83aa74108e17fd6f7b7fa6ee6
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-05-16 13:28:35 +0600

    adde testimonial

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../Component/SectionTitle/SectionTitle';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { Rating } from '@smastrom/react-rating';
+
+import '@smastrom/react-rating/style.css'
+
 const Testimonials = () => {
+      const [reviews, setReviews] = useState([])
+      useEffect(() => {
+            fetch('reviews.json')
+                  .then(res => res.json())
+                  .then(data => {
+                        setReviews(data);
+                  })
+      },[])
       return (
             <section>
                   <SectionTitle
                         subHeading='What Our Clients Say'
                         heading='TESTIMONIALS'
                   />
+                  <div>
+                        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                              
+                              {
+                                    reviews.map(review => <SwiperSlide
+                                          key={review._id}
+                                    >
+                                          
+                                          <div className='m-24 flex flex-col items-center '>
+                                                <Rating
+                                                style={{ maxWidth: 180 }}
+                                                value={review.rating}
+                                                readOnly
+                                          />
+                                                <p className='my-10'>{review.details}</p>
+                                                <h3 className='text-[#CD9003] font-medium text-3xl'>{review.name}</h3>
+                                          </div>
+
+                                    </SwiperSlide>)
+                              }
+                        </Swiper>
+                  </div>
                   
             </section>
       );
