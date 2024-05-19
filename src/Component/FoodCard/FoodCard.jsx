60ea6788b0e3a0c255c2450caf54126f394eddaf
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-05-20 01:17:45 +0600

    navbar section some work

--- src/Component/FoodCard/FoodCard.jsx
+++ src/Component/FoodCard/FoodCard.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+
+
 
 const FoodCard = ({item}) => {
       const {image, recipe, price, name } = item;
+      const handleAddToCard = (item) => {
+            console.log(item);
+      }
       return (
             <div className="card bg-base-100 shadow-xl">
                   <figure><img src={image} alt="Shoes" /></figure>
@@ -10,7 +14,9 @@ const FoodCard = ({item}) => {
                         <h2 className="card-title">{name}</h2>
                         <p>{recipe}</p>
                         <div className="card-actions justify-center">
-                              <button className="btn btn-outline border-0 border-b-4 mt-4">Add to Card</button>
+                              <button 
+                              onClick={() => handleAddToCard(item)}
+                              className="btn btn-outline border-0 border-b-4 mt-4">Add to Card</button>
                         </div>
                   </div>
             </div>
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,48 +1,86 @@
-import { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
-import { AuthContext } from '../../../Provider/AuthProvider';
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
-      const { user, logout} = useContext(AuthContext)
-      const handleLogout = () => {
-            logout()
-      }
-      const links = <>
-            <li><NavLink to='/'>Home</NavLink></li>
-            {/* <li><NavLink to='/'>CONTACT US</NavLink></li>
+  const { user, logout } = useContext(AuthContext);
+  const handleLogout = () => {
+    logout();
+  };
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      {/* <li><NavLink to='/'>CONTACT US</NavLink></li>
             <li><NavLink to='/'>DASHBOARD</NavLink></li> */}
-            <li><NavLink to='/menu'>OUR MENU</NavLink></li>
-            <li><NavLink to='/order'>OUR SHOP</NavLink></li>
-            {
-                  user ? <>
-                  <button onClick={handleLogout} className='btn'>logout</button>
-
-                  </> : <>
-                  <li><NavLink to='/login'>login</NavLink></li>
-
-                  </>
-            }
-      </>
-      return (
-            <div className="navbar fixed z-40 bg-opacity-30 bg-black text-white max-w-screen-xl">
-                  <div className="navbar-start">
-                        <div className="dropdown">
-                              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                              </div>
-                              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                    {links}
-                              </ul>
-                        </div>
-                        <h1 className="btn btn-ghost text-xl">BISTRO BOSS <br />Restaurant</h1>
-                  </div>
-                  <div className="navbar-end hidden lg:flex">
-                        <ul className="menu menu-horizontal px-1">
-                              {links}
-                        </ul>
-                  </div>
-            </div>
-      );
+      <li>
+        <NavLink to="/menu">OUR MENU</NavLink>
+      </li>
+      <li>
+        <NavLink to="/order">OUR SHOP</NavLink>
+      </li>
+      <li>
+        <Link to="/">
+          <button className="btn">
+          <FaShoppingCart />
+            <div className="badge badge-secondary">9</div>
+          </button>
+        </Link>
+      </li>
+      {user ? (
+        <>
+          <button onClick={handleLogout} className="btn">
+            logout
+          </button>
+        </>
+      ) : (
+        <>
+          <li>
+            <NavLink to="/login">login</NavLink>
+          </li>
+        </>
+      )}
+    </>
+  );
+  return (
+    <div className="navbar fixed z-40 bg-opacity-30 bg-black text-white max-w-screen-xl">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {links}
+          </ul>
+        </div>
+        <h1 className="btn btn-ghost text-xl">
+          BISTRO BOSS <br />
+          Restaurant
+        </h1>
+      </div>
+      <div className="navbar-end hidden lg:flex items-center">
+        <ul className="menu menu-horizontal  px-1">{links}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
