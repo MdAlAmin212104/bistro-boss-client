f10c4062aff676dfcc7dd03635591def0044eaf2
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-05-20 14:17:49 +0600

    cart item added and delete function added

--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,13 +1,45 @@
+import { FaTrashAlt } from "react-icons/fa";
 import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
-  const [cart] = useCart();
+  const [cart, refetch] = useCart();
   const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
-  console.log(totalPrice);
-  console.log(cart);
+  const axiosSecure = useAxiosSecure()
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+        axiosSecure.delete(`/carts/${id}`)
+         .then(res => {
+            if(res.data.deletedCount > 0){
+                refetch()
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success",
+                });
+            }
+            
+          })
+         .catch(err => console.error(err))
+      }
+    });
+  };
+
   return (
     <div>
-      <div className="flex justify-evenly mt-8">
+      <div className="flex justify-evenly my-8">
         <h2 className="text-5xl">Items : {cart.length}</h2>
         <h2 className="text-5xl">total Price : {totalPrice}</h2>
         <h2 className="text-2xl btn btn-primary">Pay</h2>
@@ -15,11 +47,9 @@ const Cart = () => {
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
-          <thead>
+          <thead className="bg-slate-600 text-white">
             <tr>
-              <th>
-                SL No
-              </th>
+              <th>#</th>
               <th>image</th>
               <th>Name</th>
               <th>price</th>
@@ -27,40 +57,35 @@ const Cart = () => {
             </tr>
           </thead>
           <tbody>
-            {
-                cart.map(item => <tr key={item._id}>
-                    <th>
-                      <label>
-                        <input type="checkbox" className="checkbox" />
-                      </label>
-                    </th>
-                    <td>
-                      <div className="flex items-center gap-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img
-                              src={item.image}
-                              alt="Avatar Tailwind CSS Component"
-                            />
-                          </div>
-                        </div>
+            {cart.map((item, idx) => (
+              <tr key={item._id}>
+                <th>{idx + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
                       </div>
-                    </td>
-                    <td>
-                      {item.name}
-                    </td>
-                    <td>{item.price}</td>
-                    <th>
-                      <button className="btn btn-ghost btn-xs">details</button>
-                    </th>
-                  </tr>)
-            }
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <th>
+                  <button
+                    onClick={() => handleDelete(item._id)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-red-400" />
+                  </button>
+                </th>
+              </tr>
+            ))}
             {/* row 1 */}
-            
-            
-            
           </tbody>
-          
         </table>
       </div>
     </div>
