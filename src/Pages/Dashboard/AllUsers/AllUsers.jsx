ddb7a525c598223f9585af3093774d4a722e6e85
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-05-23 00:06:40 +0600

    user information display

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,13 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const AllUsers = () => {
-    return (
-        <div>
-            <div className="flex justify-evenly my-6">
-                <h1 className="text-4xl">All Users</h1>
-                <h1 className="text-4xl">Total Users</h1>
-            </div>
-        </div>
-    );
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+  return (
+    <div>
+      <div className="flex justify-evenly my-6">
+        <h1 className="text-4xl">All Users</h1>
+        <h1 className="text-4xl">Total Users {users.length}</h1>
+      </div>
+      <div className="overflow-x-auto px-4">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, idx) => <tr key={user._id}>
+                <th>{idx + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>Blue</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
