3ac8bd87fa33a0da80c2376503c2d8dba6825a66
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-05-27 01:07:09 +0600

    added pamyment method and display id

--- src/Pages/Dashboard/Payment/CheckoutFrom.jsx
+++ src/Pages/Dashboard/Payment/CheckoutFrom.jsx
@@ -1,11 +1,28 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { set } from "firebase/database";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
 
 const CheckoutFrom = () => {
+  const { user } = useAuth();
   const stripe = useStripe();
   const elements = useElements();
   const [error, setError] = useState(null);
+  const [clientSecret, setClientSecret] = useState('');
+  const [transactionId, setTransactionId] = useState('');
+  const axiosSecure = useAxiosSecure();
+
+  const [cart] = useCart();
+  const totalPrice =cart.reduce((sum, item) => sum + item.price, 0);
+
+  useEffect(()=> {
+    axiosSecure.post('/create_payment_intent', {price: totalPrice})
+      .then(res => {
+        console.log(res.data.clientSecret)
+        setClientSecret(res.data.clientSecret)
+      })
+  },[axiosSecure, totalPrice])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -31,6 +48,33 @@ const CheckoutFrom = () => {
       console.log("[PaymentMethod]", paymentMethod);
       setError('');
     }
+
+
+    // confirm payment system
+    const { paymentIntent, error : confirmError} = await stripe.confirmCardPayment(clientSecret,{
+      payment_method : {
+        card : card,
+        billing_details : {
+          email : user?.email || 'anonymous',
+          name : user?.displayName || 'anonymous',
+        }
+
+      }
+    })
+
+    if(confirmError){
+      console.log('confirmation error');
+    }else{
+      console.log('paymentIntent intent', paymentIntent);
+    }
+
+    if(paymentIntent.status == 'succeeded'){
+      console.log('transaction id', paymentIntent.id);
+      setTransactionId(paymentIntent.id);
+    }
+
+
+
   };
   return (
     <div>
@@ -54,12 +98,15 @@ const CheckoutFrom = () => {
       <button
         className="btn btn-sm btn-primary my-4"
         type="submit"
-        disabled={!stripe}
+        disabled={!stripe || !clientSecret}
       >
         Pay
       </button>
     </form>
     <p className="text-red-500">{error}</p>
+    {
+      transactionId && <p className="text-green-500">Transaction Id : {transactionId}</p>
+    }
     </div>
   );
 };
