1550828064717ddb27cb2400b51ea0098f641af4
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-05-28 13:16:42 +0600

    create admin bar

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,17 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaBook, FaDollarSign, FaUsers } from "react-icons/fa";
+import { PiVanFill } from "react-icons/pi";
 
 const AdminHome = () => {
-    const { user } = useAuth()
-    return (
-        <div>
-            <h2 className="text-3xl">
-                <span>Hi, Welcome </span>
-                {
-                    user?.displayName ? user?.displayName : 'Back'
-                }
-            </h2>
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: stats = [] } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
+  return (
+    <div>
+      <h2 className="text-3xl">
+        <span>Hi, Welcome </span>
+        {user?.displayName ? user?.displayName : "Back"}
+      </h2>
+      <div className="stats shadow mt-8">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-3xl"/>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">{stats.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
-    );
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl"/>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook className="text-3xl"/>
+          </div>
+          <div className="stat-title">Product</div>
+          <div className="stat-value">{stats.products}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+          <PiVanFill className="text-3xl"/>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
